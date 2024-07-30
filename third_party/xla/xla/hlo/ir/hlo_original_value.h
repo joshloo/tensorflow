fa4baf894f969e1f922bb0745469c3587050e87e
Eugene Zhulenev <ezhulenev@google.com>
2024-07-30 11:50:44 -0700

    [xla:cpu] Optimize KernelThunk for small number of arguments and results
    name                                     old cpu/op   new cpu/op   delta
    BM_SelectAndScatterF32/128/process_time   373µs ± 2%   337µs ± 2%   -9.74%
    BM_SelectAndScatterF32/256/process_time  1.54ms ± 3%  1.39ms ± 4%  -10.04%
    BM_SelectAndScatterF32/512/process_time  7.08ms ± 7%  6.42ms ± 6%   -9.29%
    
    FUTURE_COPYBARA_INTEGRATE_REVIEW=https://github.com/openxla/xla/pull/15216 from olupton:numa-pinning dc6c68e1252fb98cb4d8b4be4dc3833a6d78494c
    PiperOrigin-RevId: 657676415

--- third_party/xla/xla/hlo/ir/hlo_original_value.h
+++ third_party/xla/xla/hlo/ir/hlo_original_value.h
@@ -13,22 +13,25 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-#include "xla/stream_executor/host_memory_allocation.h"
+#ifndef XLA_HLO_IR_HLO_ORIGINAL_VALUE_H_
+#define XLA_HLO_IR_HLO_ORIGINAL_VALUE_H_
 
-#include <cstdint>
+#include <optional>
+#include <string>
 
-#include "xla/stream_executor/stream_executor.h"
+#include "xla/shape_tree.h"
+#include "xla/shape_util.h"
 
-namespace stream_executor {
+namespace xla {
+// Stores information of original values.
+struct OriginalArray {
+  std::string instruction_name;
+  ShapeIndex shape_index;
+};
 
-HostMemoryAllocation::HostMemoryAllocation(void* ptr, uint64_t size,
-                                           StreamExecutor* executor)
-    : ptr_(ptr), size_(size), executor_(executor) {}
+using OriginalValue = ShapeTree<std::optional<OriginalArray>>;
 
-HostMemoryAllocation::~HostMemoryAllocation() {
-  if (ptr_ != nullptr && executor_ != nullptr) {
-    executor_->HostMemoryDeallocate(ptr_, size_);
-  }
-}
+std::string OriginalValueToString(const OriginalValue& original_value);
+}  // namespace xla
 
-}  // namespace stream_executor
+#endif  // XLA_HLO_IR_HLO_ORIGINAL_VALUE_H_
